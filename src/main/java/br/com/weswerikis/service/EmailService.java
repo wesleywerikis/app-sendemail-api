6d88dcda549211fe420d289a97f2d471db5e68a7
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-23 13:50:53 -0300

    feat [service-EmailService] : Implements the EmailService for sending emails

---
+++ src/main/java/br/com/weswerikis/service/EmailService.java
@@ -0,0 +1,41 @@
+package br.com.weswerikis.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.UnsupportedEncodingException;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public void sendEmail(String to, String subject, String name, String messageContent) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+        helper.setTo(to);
+        helper.setFrom("wesleyqix.1996@gmail.com", "SISTEMA DE SOFTWARE");
+        helper.setSubject(subject);
+
+        Context context = new Context();
+        context.setVariable("name", name);
+        context.setVariable("message", messageContent);
+
+        String htmlContent = templateEngine.process("emailTemplate", context);
+        helper.setText(htmlContent, true);
+
+        mailSender.send(mimeMessage);
+    }
+}
