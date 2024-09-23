5b5f334884a69e6dec2e7685575e21f5cbcf4364
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-23 14:00:38 -0300

    test [test-AppSendemailApiApplicationTests] : Adds test class to check application context loading

---
+++ src/test/java/br/com/weswerikis/AppSendemailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSendemailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
