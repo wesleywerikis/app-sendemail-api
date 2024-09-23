94c6813d9cd124fab125c941d79dc47fa782a3ed
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-23 13:57:15 -0300

    feat [start-AppSendemailApiApplication] : Creates application entry point for sending emails

---
+++ src/main/java/br/com/weswerikis/AppSendemailApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSendemailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSendemailApiApplication.class, args);
+	}
+
+}
